14e704d8944b4958e608c7e992b204afaa8748af
emarquez <iscmarquez@gmail.com>
2020-10-14 23:32:16 -0400

    configuracion

--- public/javascripts/configuration.js
+++ public/javascripts/configuration.js
@@ -18,8 +18,8 @@ $(document).ready(function(){
                 $.fn.getSpeakers();           
             }
             if(ui.newTab.index() == 3){
-                $.fn.getSpeakersConference();  
-                $.fn.getEvents(); 
+                $.fn.getAllConferences();
+               
             }             
         }
     });
@@ -113,18 +113,7 @@ $.fn.getAllEvents = function(){
     }); 
 }
 
-    $.fn.getEvents = function(){ 
-        $.ajax({
-                "url": "/PortesOuvertsConfig/configuration/outEvents",
-                "method": "GET",
-                "timeout": 0,
-            }).done(function (response) {
-                console.log(response);
-                response.forEach((item) => {
-                 $("#idEvent").append('<option value="'+item.idEvent+'">'+item.date+'</option>');
-                });
-            });
-    }
+   
 
     $("#eventModal").on("hidden.bs.modal", function () {
         $("#eventForm")[0].reset();
@@ -168,7 +157,7 @@ $.fn.getAllEvents = function(){
     }).fail(function(xhr, status, error) {
         console.log('Error - ' + JSON.stringify(xhr));
        // $("#errorMesssge").html((xhr.responseJSON.status + ":" + xhr.responseJSON.statusText));
-       $("#errorMesssge").html("Les informations ne peuvent pas être enregistrées");
+       $("#errorMesssge").html("Les informations ne peuvent pas être enregistrées/ mettre à jour");
         $("#errorAlert").show("fade");
         setTimeout(function () { 
             $("#errorAlert").alert("close"); 
@@ -246,23 +235,31 @@ $( "button[name='removeEvent']" ).click(function( event ) {
             }).done(function (response) {
                 $tableBody.clear();
                 for(let i = 0; i < response.length; i++){
-                     $tableBody.row.add([response[i].idSpeaker, response[i].photoLink, response[i].name, response[i].description,response[i].chat] );
+                    $tableBody.row.add([response[i].idSpeaker, response[i].photoLink, response[i].name, response[i].description,(response[i].chat ? "oui" : "non"),response[i].linkchat ] );
+
                 }
                 $tableBody.draw();
         }); 
     }
 
-    $.fn.getSpeakersConference = function(){ 
-        $.ajax({
-                "url": "/PortesOuvertsConfig/configuration/outSpeakerDdl",
-                "method": "GET",
-                "timeout": 0,
-            }).done(function (response) {
-                response.forEach((item) => {
-                 $("#idSpeaker").append('<option value="'+item.idSpeaker+'">'+item.name+'</option>');
-                });
-            });
-    }
+    $("#speakerModal").on("hidden.bs.modal", function () {
+        $("#speakerForm")[0].reset();
+    })
+
+    $("#speakerModal").on("show.bs.modal", function (event) {
+        var button = $(event.relatedTarget)
+        if(button.attr("name") === "addSpeaker")
+            speakerMethod="POST";
+        else if(button.attr("name") === "updateSpeaker"){
+            speakerMethod="PUT";
+            $("input[name='speakerName']").val(speakerData[2]);
+            $("input[name='speakerDescription']").val(speakerData[3]);
+            $("input[name='chat']").val(speakerData[4]);
+            $("input[name='linkchat']").val(speakerData[5]);
+
+        }  
+      })   
+
 
     $( "button[name='saveSpeaker']" ).click(function( event ) {
         event.preventDefault();
@@ -271,17 +268,15 @@ $( "button[name='removeEvent']" ).click(function( event ) {
         data.append("file",file);
         data.append("name", $("input[name='speakerName']").val());
         data.append("description", $("input[name='speakerDescription']").val());
-        if ($('#chat').is(":checked"))
-        {
-            data.append("chat","1");
-        }else 
-            data.append("chat","0");
-        console.log($("input[name='chat']:checked").val());
+        data.append("chat", ($("input[name='chat']").is(":checked") ? true : false));
+        data.append("linkchat", $("input[name='linkchat']").val());
+
         if(speakerMethod === "PUT")
             data.append("idSpeaker", $("input[name='idSpeaker']:checked").val());    
-        $.ajax({
+        console.log("tipo de post" +speakerMethod );
+            $.ajax({
             "url": "/PortesOuvertsConfig/configuration/inSpeaker",
-            "method": "POST",
+            "method": speakerMethod,
             "enctype": 'multipart/form-data',
             "processData": false,  // Important!
             "contentType": false,
@@ -290,6 +285,7 @@ $( "button[name='removeEvent']" ).click(function( event ) {
           }).done(function (response) {
               if(response.status == "success"){
                 $.fn.getSpeakers();
+                $('#eventModal').modal('hide');
                 $( "#successAlert" ).show("fade");
                 setTimeout(function () { 
                     $("#successAlert").alert("close"); 
@@ -297,7 +293,9 @@ $( "button[name='removeEvent']" ).click(function( event ) {
               }
         }).fail(function(xhr, status, error) {
             console.log('Error - ' + JSON.stringify(xhr));
-            $("#errorMesssge").html((xhr.responseJSON.status + ":" + xhr.responseJSON.statusText));
+            $('#eventModal').modal('hide');
+           // $("#errorMesssge").html((xhr.responseJSON.status + ":" + xhr.responseJSON.statusText));
+           $("#errorMesssge").html("Les informations ne peuvent pas être enregistrées/ mettre à jour");
             $("#errorAlert").show("fade");
             setTimeout(function () { 
                 $("#errorAlert").alert("close"); 
@@ -333,23 +331,126 @@ $( "button[name='removeEvent']" ).click(function( event ) {
       });
     });
 
+    // Conferences
+
+
+    var $tableConference = $("#conferenceList").DataTable({
+        'columnDefs': [{
+            'targets': 0,
+            'searchable':false,
+            'orderable':false,
+            'width':'1%',
+            'selected': true,
+            'render': function (data, type, full, meta){
+                return '<input type="radio" value="' + data + '" name="idConference">';
+            } 
+         }]
+    }); 
+    
+    $tableConference.on('click', 'tr', function () {
+        conferenceData = $tableConference.row( this ).data();
+        console.log("conferenceData : " + JSON.stringify(conferenceData));
+        let $radio = $('input[type="radio"]', $(this));
+        $radio.prop("checked", true);
+    } );
+    
+    
+    $.fn.getSpeakersConference = function(){ 
+        $.ajax({
+                "url": "/PortesOuvertsConfig/configuration/outSpeakerDdl",
+                "method": "GET",
+                "timeout": 0,
+            }).done(function (response) {
+                response.forEach((item) => {
+                 $("#idSpeaker").append('<option value="'+item.idSpeaker+'">'+item.name+'</option>');
+                });
+            });
+    }
+
+    $.fn.getAllConferences = function(){ 
+        $.ajax({
+                "url": "/PortesOuvertsConfig/configuration/inConference",
+                "method": "GET",
+                "timeout": 0,
+            }).done(function (response) {
+                $tableConference.clear();
+                for(let i = 0; i < response.length; i++){
+                    console.log(response[i]);
+                     $tableConference.row.add([response[i].idConference,response[i].nameConference, response[i].start, response[i].end, response[i].name, response[i].linkConference,response[i].idSpeaker,response[i].idEvent]);
+                }
+                $tableConference.draw();
+        }); 
+    }
+  /*  $.fn.getEvents = function(){ 
+        $.ajax({
+                "url": "/PortesOuvertsConfig/configuration/outEvents",
+                "method": "GET",
+                "timeout": 0,
+            }).done(function (response) {
+                console.log(response);
+                response.forEach((item) => {
+                 $("#idEventConference").append('<option value="'+item.idEvent+'">'+item.date+'</option>');
+                });
+            });
+    }*/
+
+    $.fn.getEventsConferences = function(){ 
+        $.ajax({
+                "url": "/PortesOuvertsConfig/configuration/Events",
+                "method": "GET",
+                "timeout": 0,
+            }).done(function (response) {
+                console.log(response);
+                response.forEach((item) => {
+                 $("#idEventConference").append('<option value="'+item.idEvent+'">'+item.date+'</option>');
+                });
+            });
+    }
+
+    $("#conferenceModal").on("hidden.bs.modal", function () {
+        $("#conferenceForm")[0].reset();
+    })
+
+    $("#conferenceModal").on("show.bs.modal", function (event) {
+        $.fn.getSpeakersConference();  
+        $.fn.getEventsConferences(); 
+        var button = $(event.relatedTarget)
+        if(button.attr("name") === "addConference")
+            conferenceMethod="POST";
+        else if(button.attr("name") === "updateConference"){
+            conferenceMethod="PUT";
+            $("#conferenceName").val(conferenceData[1]);
+            $("#commence").val(conferenceData[2]);
+            $("#end").val(conferenceData[3]);
+            $("#idSpeaker").val(conferenceData[6]);
+            $("#idEventConference").val(conferenceData[7]);
+            $("#linkConference").val(conferenceData[5]);
+            
+        }  
+    }) 
+
+
     $( "button[name='saveConference']" ).click(function( event ) {
         event.preventDefault();
+       
         $.ajax({
             "url": "/PortesOuvertsConfig/configuration/inConference",
-            "method": "POST",
+            "method": conferenceMethod,
             "timeout": 0,
             "data" : {
                 nameConference : $("input[name='conferenceName']").val(),
-                event : $("#idEvent").val(),
+                event : $("#idEventConference").val(),
                 init : $("input[name='commence']").val(),
                 end : $("input[name='end']").val(),
                 link: $("input[name='linkConference']").val(),
-                speaker: $("#idSpeaker").val()
+                speaker: $("#idSpeaker").val(),
+                idConference : conferenceMethod === "PUT" ? $("input[name='idConference']:checked").val(): null   
                                
             }
           }).done(function (response) {
                 if(response.message == "success"){
+                    $.fn.getAllConferences();
+                    $('#conferenceModal').modal('hide');
                 $( "#successAlert" ).show("fade").fadeTo(500, 0).slideUp(500, function(){
                     $(this).remove(); 
                 });
@@ -359,7 +460,8 @@ $( "button[name='removeEvent']" ).click(function( event ) {
               }
         }).fail(function(xhr, status, error) {
             console.log('Error - ' + JSON.stringify(xhr));
-            $("#errorMesssge").html((xhr.responseJSON.status + ":" + xhr.responseJSON.statusText));
+           // $("#errorMesssge").html((xhr.responseJSON.status + ":" + xhr.responseJSON.statusText));
+           $("#errorMesssge").html("Les informations ne peuvent pas être enregistrées/ mettre à jour");
             $("#errorAlert").show("fade");
             setTimeout(function () { 
                 $("#errorAlert").alert("close"); 
@@ -367,23 +469,38 @@ $( "button[name='removeEvent']" ).click(function( event ) {
       });
     });
 
-    $("#speakerModal").on("hidden.bs.modal", function () {
-        $("#speakerForm")[0].reset();
-    })
-
-    $("#speakerModal").on("show.bs.modal", function (event) {
-        var button = $(event.relatedTarget)
-        if(button.attr("name") === "addSpeaker")
-            speakerMethod="POST";
-        else if(button.attr("name") === "updateSpeaker"){
-            speakerMethod="PUT";
-            $("input[name='speakerName']").val(speakerData[2]);
-            $("input[name='speakerDescription']").val(speakerData[3]);
-
-        }  
-      })   
-
+  
     
+$( "button[name='removeConference']" ).click(function( event ) {
+    event.preventDefault();
+    $.ajax({
+        "url": "/PortesOuvertsConfig/configuration/inConference",
+        "method": "DELETE",
+        "timeout": 0,
+        "data" : {
+            idConference : conferenceData[0],             
+        }
+      }).done(function (response) {
+          if(response.message == "success"){
+              console.log("status");
+            $.fn.getAllConferences();
+            $( "#successAlert" ).show("Les informations ont été effacés");
+            setTimeout(function () { 
+                $("#successAlert").alert("close"); 
+            }, 10000); 
+          }
+    }).fail(function(xhr, status, error) {
+        console.log('Error - ' + JSON.stringify(xhr));
+       // $("#errorMesssge").html((xhr.responseJSON.status + ":" + xhr.responseJSON.statusText));
+       $("#errorMesssge").html("La conférence ne peut pas être effacé ");
+        $("#errorAlert").show("fade");
+        setTimeout(function () { 
+            $("#errorAlert").alert("close"); 
+        }, 10000);
+  });
+});
+
+
 
 
     $.fn.getGeneralConfig();
