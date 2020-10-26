d4115b55c1c08f7010f495bc87107e55e44b5298
Elizabeth Marquez <iscmarquez@gmail.com>
2020-10-26 00:12:59 -0400

    rapport

--- public/javascripts/configuration.js
+++ public/javascripts/configuration.js
@@ -655,15 +655,51 @@ $( "button[name='inscriptions']" ).click(function( event ) {
      
     $.ajax({
         "url": "/PortesOuvertsConfig/configuration/reportIns",
-        "method": "GET",
+        "method": "POST",
         "timeout": 0,
         "data" : {
             dateInitialIns : $("input[name='dateInitialIns']").val(),
             dateFinIns : $("input[name='dateFinIns']").val()               
         }
-      }).done(function (response) {
-          i
-         
+    }).done(function (response, status, xhr) {
+        console.log(response);
+        // check for a filename
+        var filename = "";
+        var disposition = xhr.getResponseHeader('Content-Disposition');
+        if (disposition && disposition.indexOf('attachment') !== -1) {
+            var filenameRegex = /filename[^;=\n]*=((['"]).*?\2|[^;\n]*)/;
+            var matches = filenameRegex.exec(disposition);
+            if (matches != null && matches[1]) filename = matches[1].replace(/['"]/g, '');
+        }
+
+        var type = xhr.getResponseHeader('Content-Type');
+        var blob = new Blob([response], { type: type });
+
+        if (typeof window.navigator.msSaveBlob !== 'undefined') {
+            // IE workaround for "HTML7007: One or more blob URLs were revoked by closing the blob for which they were created. These URLs will no longer resolve as the data backing the URL has been freed."
+            window.navigator.msSaveBlob(blob, filename);
+        } else {
+            var URL = window.URL || window.webkitURL;
+            var downloadUrl = URL.createObjectURL(blob);
+
+            if (filename) {
+                // use HTML5 a[download] attribute to specify filename
+                var a = document.createElement("a");
+                // safari doesn't support this yet
+                if (typeof a.download === 'undefined') {
+                    window.location = downloadUrl;
+                } else {
+                    a.href = downloadUrl;
+                    a.download = filename;
+                    document.body.appendChild(a);
+                    a.click();
+                }
+            } else {
+                window.location = downloadUrl;
+            }
+
+            setTimeout(function () { URL.revokeObjectURL(downloadUrl); }, 100); // cleanup
+        }
     }).fail(function(xhr, status, error) {
         
   });
@@ -674,15 +710,14 @@ $( "button[name='program']" ).click(function( event ) {
      
     $.ajax({
         "url": "/PortesOuvertsConfig/configuration/reportProg",
-        "method": "GET",
+        "method": "POST",
         "timeout": 0,
         "data" : {
             dateInitialIns : $("input[name='dateInitialProg']").val(),
             dateFinIns : $("input[name='dateFinProg']").val()               
         }
       }).done(function (response) {
-          i
-         
+
     }).fail(function(xhr, status, error) {
         
   });
